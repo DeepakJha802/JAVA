fee5dd66b0e13c1e6d5678087f77c6d11017dbd1
DeepakJha802 <dkjha7646038831@gmail.com>
2022-03-09 16:24:47 +0530

    Update

--- multiply_two_no.java
+++ multiply_two_no.java
@@ -4,9 +4,9 @@ public class multiply_two_no {
     public static void main(String[] arg)
     {
         int a, b , c ;
-        Scanner s = new Scanner(System.in);
-        a = s.nextInt();
-        b = s.nextInt();
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+        b = sc.nextInt();
         c = a * b;
 
         System.out.print(c);
