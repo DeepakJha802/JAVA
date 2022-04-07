7ce70139bdc6acbb9fc58f2683976271283c92c7
DeepakJha802 <dkjha7646038831@gmail.com>
2022-04-07 14:22:12 +0530

    Update

--- tst.java
+++ tst.java
@@ -1,26 +1,26 @@
-import java.net.SocketPermission;
+// import java.net.SocketPermission;
 
-public class tst{
-    public static void main (String[] arg)
-    {
-        char c = 'D';
-        System.out.println(c);
+// public class tst{
+//     public static void main (String[] arg)
+//     {
+//         char c = 'D';
+//         System.out.println(c);
 
-        c = 68;
-        System.out.println(c);
+//         c = 68;
+//         System.out.println(c);
 
-        c = '\104';
-        System.out.println(c);
+//         c = '\104';
+//         System.out.println(c);
 
-        c = '\u0044';
-        System.out.println(c);
+//         c = '\u0044';
+//         System.out.println(c);
 
-        String s  = "\u0044\u0045\105P\101\113";
-        System.out.println(s);
-        if (53=='5')
-        System.out.println("yes");
-        else
-        System.out.println("no");
-    }
+//         String s  = "\u0044\u0045\105P\101\113";
+//         System.out.println(s);
+//         if (53=='5')
+//         System.out.println("yes");
+//         else
+//         System.out.println("no");
+//     }
 
-}
+// }
