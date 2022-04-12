3882e56f119aab16c346a300e4ea62a6daac76db
DeepakJha802 <dkjha7646038831@gmail.com>
2022-04-12 12:08:11 +0530

    Update

--- tst.java
+++ tst.java
@@ -24,3 +24,42 @@
 //     }
 
 // }
+
+import java.util.Scanner;
+
+public class tst {
+
+    public static void main(String[] args)
+    {
+      Scanner sc = new Scanner(System.in) ;
+      String str = sc.nextLine();
+      String str1 = sc.nextLine();
+      int l = str.length();
+      int l1 = str1.length();
+      System.out.println(l+l1);
+      if((str.compareTo(str1)) > (str1.compareTo(str)))
+      {
+          System.out.println("YES");
+      }
+      else
+      {
+          System.out.println("No");
+      }
+      String string1 = str.substring(0, 1);
+      String remainingLetters = str.substring(1, str.length());
+      String string2 = str1.substring(0, 1);
+      String remainingLetters2 = str1.substring(1, str1.length());
+
+      // change the first letter to uppercase
+      string1 = string1.toUpperCase();
+      string2 = string2.toUpperCase();
+
+
+      // join the two substrings
+      str = string1 + remainingLetters;
+      str1 = string2 + remainingLetters2;
+
+      System.out.println(""+str+" "+str1+"");
+
+    }
+}
