dccd583813e59e41fe517562c1a9a5c899c5c77c
DeepakJha802 <dkjha7646038831@gmail.com>
2022-04-13 10:20:54 +0530

    Update

--- tst.java
+++ tst.java
@@ -25,41 +25,95 @@
 
 // }
 
-import java.util.Scanner;
+// import java.util.Scanner;
+
+// public class tst {
+
+//     public static void main(String[] args)
+//     {
+//       Scanner sc = new Scanner(System.in) ;
+//       String str = sc.nextLine();
+//       String str1 = sc.nextLine();
+//       int l = str.length();
+//       int l1 = str1.length();
+//       System.out.println(l+l1);
+//       if((str.compareTo(str1)) > (str1.compareTo(str)))
+//       {
+//           System.out.println("YES");
+//       }
+//       else
+//       {
+//           System.out.println("No");
+//       }
+//       String string1 = str.substring(0, 1);
+//       String remainingLetters = str.substring(1, str.length());
+//       String string2 = str1.substring(0, 1);
+//       String remainingLetters2 = str1.substring(1, str1.length());
+
+//       // change the first letter to uppercase
+//       string1 = string1.toUpperCase();
+//       string2 = string2.toUpperCase();
+
+
+//       // join the two substrings
+//       str = string1 + remainingLetters;
+//       str1 = string2 + remainingLetters2;
+
+//       System.out.println(""+str+" "+str1+"");
+
+//     }
+// }
+
+// import java.util.Scanner;
+
+// public class tst
+// {
+
+//     public static void main(String[] args)
+//     {
+//         Scanner sc = new Scanner(System.in);
+//         String str = sc.nextLine();
+//         int start = sc.nextInt();
+//         int end  = sc.nextInt();
+//             System.out.println(str.substring(start , end));
+
+
+//     }
+
+// }
+
+
+// import java.util.Scanner;
+
+// public class tst {
+
+//     public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        String str = sc.nextLine();
+//        int n = sc.nextInt();
+//        int l = str.length();
+//        System.out.println(str.substring((l-n),(l)));
+//        System.out.println(str.substring(0,(n)));
+//     }
+// }
+
+
+
 
 public class tst {
+    public static void main(String[] args){
 
-    public static void main(String[] args)
-    {
-      Scanner sc = new Scanner(System.in) ;
-      String str = sc.nextLine();
-      String str1 = sc.nextLine();
-      int l = str.length();
-      int l1 = str1.length();
-      System.out.println(l+l1);
-      if((str.compareTo(str1)) > (str1.compareTo(str)))
-      {
-          System.out.println("YES");
-      }
-      else
-      {
-          System.out.println("No");
-      }
-      String string1 = str.substring(0, 1);
-      String remainingLetters = str.substring(1, str.length());
-      String string2 = str1.substring(0, 1);
-      String remainingLetters2 = str1.substring(1, str1.length());
-
-      // change the first letter to uppercase
-      string1 = string1.toUpperCase();
-      string2 = string2.toUpperCase();
-
-
-      // join the two substrings
-      str = string1 + remainingLetters;
-      str1 = string2 + remainingLetters2;
-
-      System.out.println(""+str+" "+str1+"");
-
+       Scanner sc = new Scanner(System.in);
+       int n = sc.nextInt();
+       int arr[];
+        arr = new int[n];
+       for (int  i = 0; i<n; i++)
+       {
+           arr[i]= sc.next();
+       }
+       for (int i = 0; i<n ; i++)
+       {
+           System.out.println(arr[i]);
+       }
     }
 }
