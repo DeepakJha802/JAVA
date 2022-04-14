5d69fa37ecbba5ee1b2df9fbd18b1975c1a74228
DeepakJha802 <dkjha7646038831@gmail.com>
2022-04-14 11:03:34 +0530

    Update

--- tst.java
+++ tst.java
@@ -98,18 +98,18 @@
 // }
 
 
+import java.util.Scanner;
 
-
-public class tst {
+public class tst
+{
     public static void main(String[] args){
 
        Scanner sc = new Scanner(System.in);
        int n = sc.nextInt();
-       int arr[];
-        arr = new int[n];
+       int arr[] = new int[n];
        for (int  i = 0; i<n; i++)
        {
-           arr[i]= sc.next();
+           arr[i]= sc.nextInt();
        }
        for (int i = 0; i<n ; i++)
        {
