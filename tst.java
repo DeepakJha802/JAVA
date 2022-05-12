22b5f4ebbba0593a1def3b80a6cfe949d5ef8aa2
DeepakJha802 <dkjha7646038831@gmail.com>
2022-05-12 23:47:49 +0530

    Update

--- tst.java
+++ tst.java
@@ -187,48 +187,52 @@
 // }
 
 
-import java.util.Scanner;
-import java.lang.*;
-import java.io.*;
-import java.util.Arrays;
+// import java.util.Scanner;
+// import java.lang.*;
+// import java.io.*;
+// import java.util.Arrays;
 
-class tst {
-    public static void main(String[] args) throws java.lang.Exception {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        while (t-- > 0)
-     {
-        int n = sc.nextInt();
-        int arr[] = new int[n];
-        for(int i = 0; i < n ; i++)
-        {
-            arr[i] = sc.nextInt();
-        }
-        Arrays.sort(arr);
-        if (n == 1)
-        {
-            System.out.println(arr[0]);
-        }
-        else
-        {
-           int max = 0 , d = 0;
-           for (int i = n - 1; i >= 1; i--)
-           {
-               if (arr[i] == arr[i-1])
-               {
-                   d++;
-               }
-               else
-               {
-                   if (max < arr[i] + d)
-                   {
-                       max = arr[i] + d;
-                   }
-                   d = 0;
-               }
-           }
-           System.out.println(max);
-        }
-    }
-    }
-}
+// class tst {
+//     public static void main(String[] args) throws java.lang.Exception {
+//         Scanner sc = new Scanner(System.in);
+//         int t = sc.nextInt();
+//         while (t-- > 0)
+//      {
+//         int n = sc.nextInt();
+//         int arr[] = new int[n];
+//         for(int i = 0; i < n ; i++)
+//         {
+//             arr[i] = sc.nextInt();
+//         }
+//         Arrays.sort(arr);
+//         if (n == 1)
+//         {
+//             System.out.println(arr[0]);
+//         }
+//         else
+//         {
+//            int max = 0 , d = 0;
+//            for (int i = n - 1; i >= 1; i--)
+//            {
+//                if (arr[i] == arr[i-1])
+//                {
+//                    d++;
+//                }
+//                else
+//                {
+//                    if (max < arr[i] + d)
+//                    {
+//                        max = arr[i] + d;
+//                    }
+//                    d = 0;
+//                }
+//            }
+//            System.out.println(max);
+//         }
+//     }
+//     }
+// }
+
+
+
+
