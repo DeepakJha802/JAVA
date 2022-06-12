1e3c3efe94ba06cb1af7067a6a4d5be1707ab8f5
DeepakJha802 <dkjha7646038831@gmail.com>
2022-06-12 16:04:07 +0530

    Update

--- Kunal Lecture (Java)/Main.java
+++ Kunal Lecture (Java)/Main.java
@@ -1,12 +1,10 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {   // This is the main function in java.
         System.out.println("Hello World!");
-
+        Scanner sc = new Scanner(System.in);   //
+        System.out.println(sc.nextInt());      // here , whatever input i am getting just pass into the print statement.
     }
 }
 
-// System :- system is a class and which is contain a variable out
-// Out :- out is basically type of PrintStream. and we will learn about PrintStream later.
-// PrintStream has an object. here PrintStream has somethings called println.
 
-/// System has a variable called Out which is of type PrintStream and this Out actually has a method called println.
