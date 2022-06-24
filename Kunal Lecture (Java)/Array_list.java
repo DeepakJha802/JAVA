611a787ef3b5ceccd0757ab39451bad06402924c
DeepakJha802 <dkjha7646038831@gmail.com>
2022-06-24 16:18:05 +0530

    Update

---
+++ Kunal Lecture (Java)/Array_list.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Array_list {
+    public static void main(String[] args) {
+
+        // ArrayList<Integer> list = new ArrayList<Integer>();
+        ArrayList<Integer> list = new ArrayList<>();
+        // We can also write this way . Declareing the data type at the time of dynamic memory allocation is not mendatory.
+        {
+            
+        }
+
+    }
+
+}
--- Kunal Lecture (Java)/Arrays_multidimensional.java
+++ Kunal Lecture (Java)/Arrays_multidimensional.java
@@ -1,6 +1,40 @@
-public class Arrays_multidimensional {
-    public static void main(String[] args) {
-        
-    }
+import java.util.Scanner;
 
+public class Arrays_multidimensional {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the no of students: ");
+        int n = sc.nextInt();
+
+        int[][] arr = new int[n][];
+
+        for(int i = 0; i < n; i++)
+        {
+            System.out.print("\nEnter the no of books of Student " + i + ": ");
+            // int s = sc.nextInt();
+
+            arr[i] = new int[sc.nextInt()];
+
+            for (int j = 0; j < arr[i].length; j++)
+            {
+                System.out.print("Enter price for book " + j + ": ");
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println();
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < arr[i].length; j++)
+            {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
+
+int[] arr = new int[10];
+Student[] arr = new Student[10];
