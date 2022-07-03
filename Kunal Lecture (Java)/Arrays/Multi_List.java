1f411a2402fd48d87e8873f082f96b44860b975c
DeepakJha802 <dkjha7646038831@gmail.com>
2022-07-04 00:55:40 +0530

    Update

--- Kunal Lecture (Java)/Arrays/Multi_List.java
+++ Kunal Lecture (Java)/Arrays/Multi_List.java
@@ -16,7 +16,7 @@ public class Multi_List {
         }
 
         // Add elements.
-        fpr (int i = 0; i < 3; i++)
+        for (int i = 0; i < 3; i++)
         {
             for (int j = 0; j < 3; j++)
             {
