91c4b523329d90d2df5c8743907975d53ff8ee04
DeepakJha802 <dkjha7646038831@gmail.com>
2022-09-15 17:28:20 +0530

    Update

--- Kunal Lecture (Java)/Backtracking/Basic.java
+++ Kunal Lecture (Java)/Backtracking/Basic.java
@@ -1,12 +1,19 @@
 import java.util.Scanner;
 
+class My_String
+{
+    Scanner sc = new Scanner(System.in);
+}
+
 public class Basic
 {
     public static void main(String[] args)
     {
-        Scanner sc = new Scanner(System.in);
+        //Scanner sc = new Scanner(System.in);
 
+        My_String s1 = new My_String();
     }
 }
 
 // Backtracking :- It is basically approach to solve multiple problem.
+// Data Structure :- Data Structure basically defines the way we are going to orgenise data in my memory.
