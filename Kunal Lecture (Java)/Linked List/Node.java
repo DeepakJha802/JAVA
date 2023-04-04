48afee7705c909eea39e64e07e34d5fd0120c8ef
DeepakJha802 <aec.cse.deepak@gmail.com>
2023-04-04 11:52:28 +0530

    Linked List

--- Kunal Lecture (Java)/Linked List/Node.java
+++ Kunal Lecture (Java)/Linked List/Node.java
@@ -1,12 +1,24 @@
 
-public class Node {
+// public class Node {
 
-    int data;
-    Node next; //To store the reference the next node.
+//     int data;
+//     Node next; //To store the reference the next node.
 
-    Node (int data)  // Constructor
+//     Node (int data)  // Constructor
+//     {
+//         this.data = data;
+//         next = null;        // Default value of any reference in an any object is null.
+//     }
+// }
+
+public class Node<T> {
+
+    T data;
+    Node<T> next;
+
+    Node(T data)
     {
         this.data = data;
-        next = null;        // Default value of any reference in an any object is null.
+        next = null;
     }
 }
--- Kunal Lecture (Java)/Linked List/linked_list.java
+++ Kunal Lecture (Java)/Linked List/linked_list.java
@@ -1,11 +1,38 @@
+// public class linked_list
+// {
+//     public static void main(String[] args)
+//     {
+//         Node n1 = new Node(10);
+//         System.out.println(n1.data);
+//         System.out.println(n1.next);
+
+
+//     }
+// }
+
 public class linked_list
 {
+    public static <Integer> createLinkedList()
+    {
+        Node<Integer> n1 = new Node<>(10);
+        Node<Integer> n2 = new Node<>(20);
+        Node<Integer> n3 = new Node<>(30);
+        Node<Integer> n4 = new Node<>(40);
+
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        // n4.next = null;
+
+        return n1;
+    }
     public static void main(String[] args)
     {
-        Node n1 = new Node(10);
-        System.out.println(n1.data);
-        System.out.println(n1.next);
+        Node<Integer> head = createLinkedList();
 
-        
+        // Node<Integer> n1 = new Node<>(10);
+        // System.out.println(n1);
+        // System.out.println(n1.data);
+        // System.out.println(n1.next);
     }
 }
