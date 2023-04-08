030e65df8c0c4c53aa77a1928057f0dcacd25e4a
DeepakJha802 <aec.cse.deepak@gmail.com>
2023-04-09 02:05:57 +0530

    Take input from user in linked list

---
+++ Kunal Lecture (Java)/Linked List/take_input_from_user.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+class Node
+{
+    int data;
+    Node next;                  // To store the reference the next node.
+
+    Node (int data)           // Constructor
+    {
+        this.data = data;
+        next = null;        // Default value of any reference in an any object is null.
+    }
+}
+public class take_input_from_user {
+
+    public static Node takeInput()
+    {
+        Scanner sc = new Scanner(System.in);
+
+        Node head = null;
+        Node tail = null;
+
+        while (true)
+        {
+            int data = sc.nextInt();
+            if (data == -1)
+            {
+                break;
+            }
+
+            Node currentNode = new Node(data);
+
+            if (head == null)
+            {
+                head = currentNode;
+                tail = currentNode;
+            }
+
+            else
+            {
+                tail.next = currentNode;
+                tail = currentNode;
+            }
+        }
+        return head;
+    }
+    public static void print(Node head)
+    {
+        while (head != null)
+        {
+            System.out.print(head.data + " ");
+            head = head.next;
+        }
+    }
+    public static void main(String[] args)
+    {
+        Node head = takeInput();
+        print(head);
+    }
+}
