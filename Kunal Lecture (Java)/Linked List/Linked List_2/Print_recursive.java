4ae4663e021660cbed43c8f3217baef0c28269a6
DeepakJha802 <aec.cse.deepak@gmail.com>
2023-04-14 13:40:35 +0530

    Some changes

---
+++ Kunal Lecture (Java)/Linked List/Linked List_2/Print_recursive.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Node
+{
+    int data;
+    Node next;
+
+    Node (int data)
+    {
+        this.data = data;
+        next = null;
+    }
+}
+
+public class Print_recursive
+{
+    
+}
